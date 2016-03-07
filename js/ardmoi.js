e9051de3f2fbf18d2131e6dfb2b106783dbb1ffc
flowennl <lowen.f@gmail.com>
2016-03-08 00:09:54 +0100

    strobo's added, new loop for aphex twin

--- js/ardmoi.js
+++ js/ardmoi.js
@@ -3,48 +3,53 @@
 //init variables
 var $loader = $('.loader'),
     $btnNewSong = $('.new-song');
+
 // TODO find right frequences and tresholds for all songs
 var songs = [
             {
-                filename: 'sample-aphex.mp3',
-                fbass: { x1:0, x2:60, t: .8},
-                fmid: { x1:70, x2:120, t: .7}
+                filename: 'sample-aphex-l.mp3',
+                fbass: { x1:0, x2:60, t: 0.8},
+                fmid: { x1:70, x2:120, t: 0.7}
             },
             {
                 filename: 'sample-priest.mp3',
-                fbass: { x1:0, x2:90, t: .7},
-                fmid: { x1:91, x2:140, t: .8}
+                fbass: { x1:0, x2:90, t: 0.7},
+                fmid: { x1:91, x2:140, t: 0.8}
             },
             {
                 filename: 'sample-jamie.mp3',
-                fbass: { x1:0, x2:60, t: .7},
-                fmid: { x1:70, x2:120, t: .7}
-            },
-            {
-                filename: 'sample-impala.mp3',
-                fbass: { x1:40, x2:70, t: .7},
-                fmid: { x1:70, x2:120, t: .7}
+                fbass: { x1:0, x2:60, t: 0.7},
+                fmid: { x1:90, x2:110, t: 0.7}
             }
+            // {
+            //     filename: 'sample-impala.mp3',
+            //     fbass: { x1:40, x2:70, t: 0.7},
+            //     fmid: { x1:70, x2:110, t: 0.9}
+            // }
             ];
 
 var images = ['berlin.jpg','flying.jpg','ganesh.jpg','japan.jpg','moondark.jpg', 'moonlight.jpg', 'nihon.jpg', 'space.jpg', 'terminator.jpg'];
 
-var fft = new p5.FFT()
+var fft = new p5.FFT();
 
 var volume = 1,   
-    fadeout = 1,  // fadeout on scroll
-    dmapX = 1;    // move X dmap
+    fadeout = 1; // fadeout on scroll
 
-var currentSong,
-    thisSong, fbass, fmid, bassDetect, midDetect,
-    currentImage;
+var currentSong = 0,  // if random, remove default value
+    sound, thisSong,
+    fbass, fmid, bassDetect, midDetect,
+    currentImage,
+    mainVisual, displacementFilter, displacementTexture, imageInterval, 
+    stage, renderer;
 
+var windowWidth = window.innerWidth,
+    windowHeight = window.innerHeight;
 /*
     Assetloader to preload all sprites
 */
 var assLoader = PIXI.loader;
 
-for (i=0; i<images.length; i++) {
+for (var i = 0; i < images.length; i++) {
     assLoader.add(images[i],'images/'+ images[i]);
     // HOWTO ADD DEPTH MAPS?
     // assLoader.add('depth-'+ images[i],'dmaps/'+ images[i]);
@@ -54,8 +59,8 @@ for (i=0; i<images.length; i++) {
     preload Sound
 */
 function preload(song) {
-    // console.log('preloading song: ');
-    // console.log(song.filename);
+    console.log('preloading song: ' + currentSong);
+    console.log(song.filename);
     sound = new p5.SoundFile('songs/' + song.filename,
         onMusicLoaded,
         h.onError
@@ -71,7 +76,6 @@ function onMusicLoaded() {
     sound.play();
 }
 
-
 /*
     play random song
     fires on 'onended'
@@ -80,14 +84,20 @@ function newSong() {
     var randomSong = h.getRandomInt(0, songs.length-1);
 
     // let's prevent same song
-    if (randomSong === currentSong) {
-        newSong();
-    } else {
-        currentSong = randomSong;
-        preload(songs[currentSong]);
-    }
+    // if (randomSong === currentSong) {
+    //     newSong();
+    // } else {
+    //     currentSong = randomSong;
+    //     preload(songs[currentSong]);
+    // }
     
-    // let's set the parameters for bass and mid detection
+    preload(songs[currentSong]);
+    if (currentSong < songs.length-1) {
+        currentSong++;
+    } else {
+        currentSong = 0;
+    }
+    // // let's set the parameters for bass and mid detection
     thisSong = songs[currentSong];
     fbass = thisSong.fbass;
     fmid = thisSong.fmid;
@@ -122,21 +132,17 @@ function getSpectrum() {
     }
 }
 
-var overlay = $('.canvas-overlay');
-var tl = new TimelineLite();
+/*
+    STROBO'S
+*/
+var overlay = $('.canvas-overlay'),
+    tl = new TimelineLite();
 function stroboBlack() {
-    console.log('stroboBlack');
-    // mainVisual.filters = [displacementFilter, bloomFilter];
-    
-
     tl.to(overlay, .07, {opacity: 1})
       .to(overlay, .01, {opacity: 0});
 }
 
 function stroboWhite() {
-    console.log('white');
-    // mainVisual.filters = [displacementFilter];
-
     tl.to(overlay, .07, {opacity: 1, backgroundColor: '#fff'})
       .to(overlay, .01, {opacity: 0, backgroundColor: '#000'});
 }
@@ -147,8 +153,6 @@ function stroboWhite() {
 function initPixiContainer() {
     console.log('initPixiContainer');
     stage = new PIXI.Container();
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight * .9; // TODO: set ratio 16:10 for width:height
     renderer = new PIXI.autoDetectRenderer(
         windowWidth,
         windowHeight,
@@ -163,23 +167,14 @@ function initPixiContainer() {
     var images = assLoader.resources,
         imageKeys = Object.keys(images);
 
-
-    function compare(a, b, c) {
-        if (a === b) {
-            c();
-        } else {
-            a = b;
-            preload(songs[currentSong]);
-        }
-    }
-
     function newImage() {
-        // console.log('change image');
-        // console.log('--------------------------');
+        console.log('change image');
+        console.log('--------------------------');
         // image and image depth map have the same name, but different folders.
 
         var randomImage = h.getRandomInt(0, imageKeys.length-1),
             thisImage = images[imageKeys[randomImage]];
+        console.log(thisImage.name);
 
         if (randomImage === currentImage) {
             newImage();
@@ -195,13 +190,12 @@ function initPixiContainer() {
         //DMAP
         displacementTexture = new PIXI.Sprite.fromImage('dmaps/' + thisImage.name);
         stage.addChild(displacementTexture);
-        // console.log(displacementTexture._texture.baseTexture.imageUrl);
+        console.log(displacementTexture._texture.baseTexture.imageUrl);
         displacementFilter = new PIXI.filters.DisplacementFilter(displacementTexture);
         
-        bloomFilter = new PIXI.filters.BloomFilter();
-
         // main visual randomized
         mainVisual = new PIXI.Sprite.fromImage(thisImage.url);
+        mainVisual.width = windowWidth;
         stage.addChild(mainVisual);
     
         // add filters
@@ -223,17 +217,21 @@ function initPixiContainer() {
 function animate() {
     // center mainVisual
     var mainVisualX = (windowWidth - mainVisual.width) / 2;
+    var mainVisualY = (windowHeight - mainVisual.height) / 2;
     mainVisual.position.x = mainVisualX;
+    mainVisual.position.y = mainVisualY;
 
     // center displacementMap
     var dmapX = (windowWidth - displacementTexture.width) / 2;
+    var dmapY = (windowHeight - displacementTexture.height) / 2;
     displacementTexture.position.x = dmapX;
+    displacementTexture.position.y = dmapY;
 
     // move displacement map around
     displacementFilter.scale.x += 1.25;
 
     getSpectrum();
-    // displacementFilter.scale.x -= 5;
+    displacementFilter.scale.x -= 5;
 
     // Loops never end though!
     sound.onended(function() {
